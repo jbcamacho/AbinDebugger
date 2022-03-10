c2edf9eebec910370fccd3e6d217a02e77aa6e1a
Juan Camacho <jbcamacho01@gmail.com>
2022-03-09 18:41:43 -0600

    added new benchmark check_password_strength2

--- benchmarks/PasswordStrength.py
+++ benchmarks/PasswordStrength.py
@@ -11,6 +11,17 @@ Given a string representing a password, this benchmark checks the password stren
             "Function": ["check_password_strength"],
             "Bug": [ {"Position": 38, "LOC": "whitespace_count = 1"} ],
             "Fix": [ {"Position": 38, "LOC": "whitespace_count += 1"} ]
+        },
+        {
+            "Function": ["check_password_strength2"],
+            "Bug": [
+                {"Position": 90, "LOC": "whitespace_count = 1"},
+                {"Position": 92, "LOC": "special_char_count = 1"}
+            ],
+            "Fix": [
+                {"Position": 90, "LOC": "whitespace_count += 1"},
+                {"Position": 92, "LOC": "special_char_count += 1"}
+            ]
         }
     ]
 }
@@ -64,3 +75,45 @@ def check_password_strength(password):
         remarks = "Now that's one hell of a strong password !!! Hackers don't have a chance guessing that password."
     
     return (strength, lower_alpha_count, upper_alpha_count, number_count, whitespace_count, special_char_count)
+
+
+def check_password_strength2(password):
+    lower_alpha_count = upper_alpha_count = number_count = whitespace_count = special_char_count = 0
+    for char in list(password):
+        if char in string.ascii_lowercase:
+            lower_alpha_count += 1
+        elif char in string.ascii_uppercase:
+            upper_alpha_count += 1
+        elif char in string.digits:
+            number_count += 1
+        elif char == ' ':
+            whitespace_count = 1 # <-- FIX whitespace_count += 1
+        else:
+            special_char_count = 1 # <-- FIX special_char_count += 1
+    strength = 0
+    remarks = ''
+
+    if lower_alpha_count >= 1:
+        strength += 1
+    if upper_alpha_count >= 1:
+        strength += 1
+    if number_count >= 1:
+        strength += 1
+    if whitespace_count >= 1:
+        strength += 1
+    if special_char_count >= 1:
+        strength += 1
+
+    if strength == 1:
+        remarks = "That's a very bad password. Change it as soon as possible."
+    elif strength == 2:
+        remarks = "That's not a good password. You should consider making a tougher password."
+    elif strength == 3:
+        remarks = "Your password is okay, but it can be improved a lot"
+    elif strength == 4:
+        remarks = "Your password is hard to guess. But you can make it even more secure"
+    elif strength == 5:
+        remarks = "Now that's one hell of a strong password !!! Hackers don't have a chance guessing that password."
+    
+    return (strength, lower_alpha_count, upper_alpha_count, number_count, whitespace_count, special_char_count)
+
