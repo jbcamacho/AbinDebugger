05eb71f7705540efd8febac32010323e24250ca5
Juan Camacho <jbcamacho01@gmail.com>
2022-05-26 21:52:33 -0500

    Added benchmark 'NQueens5'

--- benchmarks/NQueens.py
+++ benchmarks/NQueens.py
@@ -50,6 +50,29 @@ def permutations(iterable, r=None):
         else:
             return
 
+# Pure-Python implementation of itertools.permutations().
+def permutations2(iterable, r=None):
+    """permutations(range(3), 2) --> (0,1) (0,2) (1,0) (1,2) (2,0) (2,1)"""
+    pool = tuple(iterable)
+    n = len(pool)
+    if r is None:
+        r = n
+    indices = list(range(n))
+    cycles = list(range(n - r + 1, n + 1))[::-1]
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i + 1:] + indices[i:i + 1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
 
 # Pure-Python implementation of itertools.permutations().
 def permutations3(iterable, r=None):
@@ -69,6 +92,30 @@ def permutations3(iterable, r=None):
                 cycles[i] = n - i
             else:
                 j = cycles[i,:] # <-- FIX break j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                return # <-- FIX break
+        else:
+            return
+
+# Pure-Python implementation of itertools.permutations().
+def permutations4(iterable, r=None):
+    """permutations(range(3), 2) --> (0,1) (0,2) (1,0) (1,2) (2,0) (2,1)"""
+    pool = tuple(iterable)
+    n = len(pool)
+    if r is None:
+        r = n
+    indices = list(range(n))
+    cycles = list(range(n - r + 1, n + 1))[::-1]
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i + 1:] + indices[i:i + 1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i] 
                 indices[i], indices[-j] = indices[-j], indices[i]
                 yield tuple(pool[i] for i in indices[:r])
                 return # <-- FIX break
@@ -130,6 +177,42 @@ def n_queens3(queen_count):
                         == len(set(vec[i] - i for i in cols))):
             yield vec
 
+def n_queens4(queen_count):
+    """N-Queens solver.
+
+    Args:
+        queen_count: the number of queens to solve for. This is also the
+            board size.
+
+    Yields:
+        Solutions to the problem. Each yielded value is looks like
+        (3, 8, 2, 1, 4, ..., 6) where each number is the column position for the
+        queen, and the index into the tuple indicates the row.
+    """
+    cols = range(queen_count+1) # <-- FIX cols = range(queen_count)
+    for vec in permutations2(cols):
+        if (queen_count == len(set(vec[i] + i for i in cols))
+                        == len(set(vec[i] - i for i in cols))):
+            return vec # <-- FIX yield vec
+
+def n_queens5(queen_count):
+    """N-Queens solver.
+
+    Args:
+        queen_count: the number of queens to solve for. This is also the
+            board size.
+
+    Yields:
+        Solutions to the problem. Each yielded value is looks like
+        (3, 8, 2, 1, 4, ..., 6) where each number is the column position for the
+        queen, and the index into the tuple indicates the row.
+    """
+    cols = range(queen_count)
+    for vec in permutations4(cols):
+        if (queen_count == len(set(vec[i] + i for i in cols))
+                        == len(set(vec[i] - i for i in cols))):
+            return vec # <-- FIX yield vec
+
 def aBench_n_queens(queen_count):
     return list(n_queens(queen_count))
 
@@ -138,6 +221,12 @@ def aBench_n_queens2(queen_count):
 
 def aBench_n_queens3(queen_count):
     return list(n_queens3(queen_count))
+
+def aBench_n_queens4(queen_count):
+    return list(n_queens4(queen_count))
+
+def aBench_n_queens5(queen_count):
+    return list(n_queens5(queen_count))
 
 if __name__ == "__main__":
     """runner = pyperf.Runner()
